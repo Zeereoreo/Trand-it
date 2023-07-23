b904cb172d61d83c1bee946a9704e5887e4bfe84
Zeereoreo <wltjrcjf12@naver.com>
2023-07-23 22:25:08 +0900

    search

--- trand-it/pages/index.tsx
+++ trand-it/pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import { Mongoose } from "mongoose";
+import styled from 'styled-components';
 
 declare global {
   var mongoose: {
@@ -9,11 +10,17 @@ declare global {
   };
 }
 
+const Header = styled.h2`
+  margin: 0;
+  text-align: center;
+`
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Trand-it</h1>
+    <Header>Trand-it</Header>
     <p>
-      <Link href="/about">About</Link>
+      {/* 서치 */}
+      <Link href="/search">search</Link>
     </p>
   </Layout>
 )
