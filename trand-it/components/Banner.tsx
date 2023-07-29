999d93c6863cc3f59c7304f3c4c4d939dc7f7982
Zeereoreo <wltjrcjf12@naver.com>
2023-07-29 16:28:27 +0900

    fix : Header

--- trand-it/components/Banner.tsx
+++ trand-it/components/Banner.tsx
@@ -1,3 +1,5 @@
+
+
 export default function Banner() {
     return (
         <>
--- trand-it/pages/index.tsx
+++ trand-it/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import { Mongoose } from "mongoose";
 import styled from 'styled-components';
-
+import Banner from '../components/Banner';
 
 declare global {
   var mongoose: {
@@ -22,6 +22,7 @@ const IndexPage = () => (
       {/* 서치 */}
       {/* <Header>Trand-it</Header> */}
       <Link href="/search">search</Link>
+      <Banner></Banner>
     </p>
   </Layout>
 )
